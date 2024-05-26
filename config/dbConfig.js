cbec9b03028d0706d792e41019746d5f7ce1a539
akashm <akashmadali2001@gmail.com>
2024-05-26 10:40:32 +0530

    adding some middleware

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,10 +1,10 @@
-
+require('dotenv').config();
 module.exports ={
-    HOST : 'localhost',
-    USER : 'root',
-    PASSWORD  : 'Root@123',
-    DB : 'expense_tracker',
-    dialect : 'mysql',
+    HOST : process.env.DB_HOST,
+    USER : process.env.DB_USER,
+    PASSWORD  : process.env.DB_PASSWORD,
+    DB : process.env.DB_NAME,
+    dialect : process.env.DB_TYPE,
 
     pool : {
         max : 5 ,
--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const cors = require('cors');
 const multer = require('multer');
 const upload = multer();
-
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
 const app = express();
+const https = require('https');
 
 app.use(cors({
     origin: 'http://localhost:5050',
 }));
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
 
 
 //middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(upload.array());
-
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
 
 //routers 
 const router = require('./routes/homeRoutes');
@@ -24,7 +35,12 @@ app.use('/api', router);
 //port
 const PORT = process.env.PORT || 8080;
 
+// const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app);
 
+// // Start server
+// httpsServer.listen(PORT, () => {
+//     console.log(`HTTPS Server is running on port ${PORT}`);
+// });
 //server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
