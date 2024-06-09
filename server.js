8daaea97af1ca5e56b2aa27b9ee36f1ddb984fce
akashm <akashmadali2001@gmail.com>
2024-06-09 21:46:07 +0530

    changes

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.use((req,res) => {
 })
 
 //port 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 // const httpsServer = https.createServer({ key: privateKey, cert: certificate }, app);
 
